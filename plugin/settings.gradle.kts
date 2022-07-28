11a7987aed37d48131c7328cd2ee105b4e3124d2
irgaly <irgaly@gmail.com>
2022-07-28 20:44:19 +0900

    require kotest plugin

---
+++ plugin/settings.gradle.kts
@@ -0,0 +1,24 @@
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        kotlin("jvm") version "1.6.21"
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+rootProject.name = "plugin"
