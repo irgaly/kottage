7baaaefcbe71d0e87e0e875f89602c6720fcd014
irgaly <irgaly@gmail.com>
2022-08-28 01:36:36 +0900

    Add Debug Test

--- kottage/src/commonTest/kotlin/io/github/irgaly/kottage/KottageTest.kt
+++ kottage/src/commonTest/kotlin/io/github/irgaly/kottage/KottageTest.kt
@@ -4,16 +4,61 @@ import com.soywiz.klock.DateTime
 import io.github.irgaly.kottage.platform.Context
 import io.github.irgaly.kottage.platform.TestCalendar
 import io.github.irgaly.test.extension.tempdir
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldNotBeEmpty
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class KottageTest : DescribeSpec({
     val tempDirectory = tempdir()
     val calendar = TestCalendar(DateTime(2022, 1, 1).utc)
-    println("tempDirectory = $tempDirectory")
     describe("Kottage") {
+        context("debug 機能") {
+            val kottage = Kottage(
+                "test",
+                tempDirectory,
+                KottageEnvironment(Context(), calendar)
+            )
+            it("tempDirectory 表示") {
+                println("tempDirectory = $tempDirectory")
+            }
+            it("getDatabaseStatus() で情報を取得できる") {
+                val status = kottage.getDatabaseStatus()
+                println(status)
+                status.shouldNotBeEmpty()
+            }
+            it("compact() をエラーなく実行できる") {
+                shouldNotThrowAny {
+                    kottage.compact()
+                }
+            }
+            it("export() でバックアップを作成できる") {
+                kottage.export("backup.db", tempDirectory)
+            }
+            it("export() で存在しないディレクトリにバックアップを作成できる") {
+                kottage.export("backup.db", "$tempDirectory/backup")
+            }
+            // TODO:
+            // シングルクォート、ダブルクォーテーション
+            // バックスラッシュ、スペース、: の扱いを確認する
+            // ファイル名に /, \, シングルクォート、ダブルクォーテーションの扱いを確認する
+        }
+        context("Connection") {
+            val directory = "$tempDirectory/subdirectory"
+            val kottage = Kottage(
+                "test",
+                directory,
+                KottageEnvironment(Context(), calendar)
+            )
+            val storage = kottage.storage("storage1")
+            it("ディレクトリが存在しなくてもファイルを作成できる") {
+                shouldNotThrowAny {
+                    storage.put("key", "test")
+                }
+            }
+        }
         context("storage モード") {
             val kottage = Kottage(
                 "test",
