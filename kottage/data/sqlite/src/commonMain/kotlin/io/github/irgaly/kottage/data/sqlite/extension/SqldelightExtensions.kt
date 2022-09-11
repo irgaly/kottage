12a5159802941a419098382ddb24a36320a9f5e8
irgaly <irgaly@gmail.com>
2022-09-12 00:00:18 +0900

    fix executeAsExists(): cursor leak

--- kottage/data/sqlite/src/commonMain/kotlin/io/github/irgaly/kottage/data/sqlite/extension/SqldelightExtensions.kt
+++ kottage/data/sqlite/src/commonMain/kotlin/io/github/irgaly/kottage/data/sqlite/extension/SqldelightExtensions.kt
@@ -1,10 +1,11 @@
 package io.github.irgaly.kottage.data.sqlite.extension
 
 import com.squareup.sqldelight.Query
+import com.squareup.sqldelight.db.use
 
 /**
  * execute query and result has one more items
  */
 fun Query<*>.executeAsExists(): Boolean {
-    return execute().next()
+    return execute().use { it.next() }
 }
