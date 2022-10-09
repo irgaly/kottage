8302eaa29c797aec626ecd5920e99d930b2d3e12
irgaly <irgaly@gmail.com>
2022-10-10 02:42:37 +0900

    add KottageMigrationTest

--- kottage/data/sqlite/src/commonMain/kotlin/io/github/irgaly/kottage/data/sqlite/KottageDatabase1.kt
+++ kottage/data/sqlite/src/commonMain/kotlin/io/github/irgaly/kottage/data/sqlite/KottageDatabase1.kt
@@ -41,7 +41,6 @@ class KottageDatabase1(
             driver.execute(
                 null, """
           |CREATE TABLE item (
-          |  -- "{type}+{key}"
           |  key TEXT PRIMARY KEY,
           |  type TEXT NOT NULL,
           |  string_value TEXT,
@@ -68,6 +67,31 @@ class KottageDatabase1(
             driver.execute(null, "CREATE INDEX item_type_created_at ON item(type, created_at)", 0)
             driver.execute(null, "CREATE INDEX item_type_last_read ON item(type, last_read_at)", 0)
             driver.execute(null, "CREATE INDEX item_type_expire_at ON item(type, expire_at)", 0)
+            driver.execute(
+                null,
+                "INSERT INTO item_stats(item_type, count, event_count) VALUES('cache1', 1, 1)",
+                0
+            )
+            driver.execute(
+                null,
+                // 1640995200000 = 2022/1/1 00:00:00.000 UTC
+                // 1643587200000 = 2022/1/31 00:00:00.000 UTC
+                "INSERT INTO item_event(id, created_at, expire_at, item_type, item_key, event_type) VALUES('61b658c4250a4e9a9d07a9815655c5e1', 1640995200000, 1643587200000, 'cache1', 'cache1+key1', 'Create')",
+                0
+            )
+            driver.execute(
+                null,
+                // 1640995200000 = 2022/1/1 00:00:00.000 UTC
+                "INSERT INTO stats(key, last_evict_at) VALUES('kottage', 1640995200000)",
+                0
+            )
+            driver.execute(
+                null,
+                // 1640995200000 = 2022/1/1 00:00:00.000 UTC
+                // 1643587200000 = 2022/1/31 00:00:00.000 UTC
+                "INSERT INTO item(key, type, string_value, long_value, double_value, bytes_value, created_at, last_read_at, expire_at) VALUES('cache1+key1', 'cache1', 'value1', NULL, NULL, NULL, 1640995200000, 1640995200000, 1643587200000)",
+                0
+            )
         }
 
         override fun migrate(
