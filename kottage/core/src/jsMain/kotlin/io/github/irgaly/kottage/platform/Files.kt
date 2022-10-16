e73126dcf329e438392a01083812792056cf891b
irgaly <irgaly@gmail.com>
2022-10-17 00:16:34 +0900

    fix js: Files dummy

--- kottage/core/src/jsMain/kotlin/io/github/irgaly/kottage/platform/Files.kt
+++ kottage/core/src/jsMain/kotlin/io/github/irgaly/kottage/platform/Files.kt
@@ -3,11 +3,11 @@ package io.github.irgaly.kottage.platform
 actual class Files {
     actual companion object {
         actual fun exists(path: String): Boolean {
-            TODO()
+            throw UnsupportedOperationException("browser js cannot access to File Storage")
         }
 
         actual fun mkdirs(directoryPath: String): Boolean {
-            TODO()
+            throw UnsupportedOperationException("browser js cannot access to File Storage")
         }
 
         actual val separator: String = "/"
