32833c2932c5bd2d44a2c6a74bd90a7a81e41ada
irgaly <irgaly@gmail.com>
2022-10-31 18:40:40 +0900

    fix internal model classes

--- kottage/src/commonMain/kotlin/io/github/irgaly/kottage/internal/model/ItemListStats.kt
+++ kottage/src/commonMain/kotlin/io/github/irgaly/kottage/internal/model/ItemListStats.kt
@@ -1,6 +1,6 @@
 package io.github.irgaly.kottage.internal.model
 
-data class ItemListStats(
+internal data class ItemListStats(
     val listType: String,
     val count: Long,
     val firstItemPositionId: String,
