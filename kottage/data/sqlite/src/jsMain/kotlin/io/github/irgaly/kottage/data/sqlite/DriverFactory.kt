e72cdfa548cd7624467992a770d0aff85a8e9c5e
irgaly <irgaly@gmail.com>
2022-11-07 00:10:20 +0900

    apply sqlite3 connection settings

--- kottage/data/sqlite/src/jsMain/kotlin/io/github/irgaly/kottage/data/sqlite/DriverFactory.kt
+++ kottage/data/sqlite/src/jsMain/kotlin/io/github/irgaly/kottage/data/sqlite/DriverFactory.kt
@@ -3,6 +3,7 @@ package io.github.irgaly.kottage.data.sqlite
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.db.use
 import io.github.irgaly.kottage.data.sqlite.external.DatabaseConstructor
+import io.github.irgaly.kottage.data.sqlite.external.Options
 import io.github.irgaly.kottage.platform.Context
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -16,7 +17,33 @@ actual class DriverFactory actual constructor(
         directoryPath: String,
         schema: SqlDriver.Schema
     ): SqlDriver {
-        val driver = NodejsSqlDriver(betterSqlite3Database("$directoryPath/$fileName"))
+        // better-sqlite3
+        // * busy_timeout = 5000 (ms, default)
+        //   * 3000 (ms) に設定
+        // * journal_mode = DELETE (default)
+        //   * DELETE に設定
+        // * journal_size_limit = -1 (default)
+        //   * 524288 bytes = 512 KB に設定
+        // * synchronous = FULL (FULL, default)
+        //   * NORMAL に設定
+        // * cache_size = -16000 (KB, default)
+        //   * -2000 (KB) に設定
+        // * threading mode = multi-thread
+        //   * https://github.com/WiseLibs/better-sqlite3/blob/d66747ef563dc39fd038e0ec60d91c9084e3e62c/docs/compilation.md
+        //   * JavaScript なのでシングルスレッドであり serialized は不要
+        val driver = NodejsSqlDriver(
+            betterSqlite3Database(
+                "$directoryPath/$fileName",
+                js("{}").unsafeCast<Options>().apply {
+                    timeout = 3000
+                }
+            )
+        ).apply {
+            db.pragma("journal_mode = WAL")
+            db.pragma("journal_size_limit = 524288")
+            db.pragma("synchronous = NORMAL")
+            db.pragma("cache_size = -2000")
+        }
         migrateIfNeeded(driver, schema)
         return driver
     }
