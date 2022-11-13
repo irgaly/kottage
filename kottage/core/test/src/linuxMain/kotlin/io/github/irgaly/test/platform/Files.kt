d0d07795b298747a1363ab05caa1336a44a8b0e2
irgaly <irgaly@gmail.com>
2022-11-14 01:25:21 +0900

    implement Files on Linux

--- kottage/core/test/src/linuxMain/kotlin/io/github/irgaly/test/platform/Files.kt
+++ kottage/core/test/src/linuxMain/kotlin/io/github/irgaly/test/platform/Files.kt
@@ -1,14 +1,37 @@
 package io.github.irgaly.test.platform
 
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.staticCFunction
+import kotlinx.cinterop.toKString
+import platform.posix.FTW_DEPTH
+import platform.posix.FTW_PHYS
+import platform.posix.getenv
+import platform.posix.mkdtemp
+import platform.posix.nftw
+import platform.posix.remove
+
 actual class Files {
     actual companion object {
         actual fun createTemporaryDirectory(): String {
-            throw NotImplementedError()
+            val tempDirectory =
+                sequenceOf("TMPDIR", "TMP", "TEMP", "TEMPDIR").firstNotNullOfOrNull {
+                    getenv(it)?.toKString()
+                } ?: "/tmp"
+            val directory = mkdtemp("$tempDirectory/tmpdir.XXXXXX".cstr)?.toKString()
+            return checkNotNull(directory)
         }
 
 
         actual fun deleteRecursively(directoryPath: String): Boolean {
-            throw NotImplementedError()
+            val ret = nftw(
+                directoryPath,
+                staticCFunction { pathName, _, _, _ ->
+                    remove(pathName!!.toKString())
+                },
+                64,
+                FTW_DEPTH or FTW_PHYS
+            )
+            return (ret != -1)
         }
     }
 }
