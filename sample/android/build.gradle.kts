4e5f36959c81c2a1cdd3cff0fedad803d3e91b49
irgaly <irgaly@gmail.com>
2022-12-14 22:53:25 +0900

    update android sample: add compose

--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -8,13 +8,21 @@ android {
     defaultConfig {
         applicationId = "io.github.irgaly.kottage.sample"
         versionName = libs.versions.kottage.get()
+        minSdk = 21
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
 }
 
 dependencies {
+    implementation(dependencies.platform(libs.compose.bom))
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle)
-    implementation(libs.androidx.material)
+    implementation(libs.bundles.compose)
     implementation(libs.kotlinx.serialization)
     implementation(projects.kottage)
 }
--- sample/android/src/main/kotlin/io/github/irgaly/kottage/sample/ui/MainActivity.kt
+++ sample/android/src/main/kotlin/io/github/irgaly/kottage/sample/ui/MainActivity.kt
@@ -6,26 +6,17 @@ import androidx.lifecycle.lifecycleScope
 import io.github.irgaly.kottage.Kottage
 import io.github.irgaly.kottage.KottageEnvironment
 import io.github.irgaly.kottage.get
-import io.github.irgaly.kottage.platform.KottageCalendar
 import io.github.irgaly.kottage.platform.contextOf
 import io.github.irgaly.kottage.put
-import io.github.irgaly.kottage.sample.R
 import kotlinx.coroutines.launch
 
-class MainActivity: AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val kottage = Kottage(
             "test",
             cacheDir.absolutePath,
-            KottageEnvironment(
-                contextOf(baseContext),
-                object : KottageCalendar {
-                    override fun nowUnixTimeMillis(): Long {
-                        return System.currentTimeMillis()
-                    }
-                }
-            )
+            KottageEnvironment(contextOf(baseContext))
         )
 
         val storage = kottage.storage("test")
