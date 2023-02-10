14f023969c650e4c657fd622925cd34298ef13d9
irgaly <irgaly@gmail.com>
2023-02-10 22:24:51 +0900

    add KottageLogger in Test

--- kottage/src/commonTest/kotlin/io/github/irgaly/kottage/extension/Extensions.kt
+++ kottage/src/commonTest/kotlin/io/github/irgaly/kottage/extension/Extensions.kt
@@ -6,6 +6,7 @@ import io.github.irgaly.kottage.Kottage
 import io.github.irgaly.kottage.KottageEnvironment
 import io.github.irgaly.kottage.KottageOptions
 import io.github.irgaly.kottage.platform.KottageContext
+import io.github.irgaly.kottage.platform.KottageLogger
 import io.github.irgaly.kottage.platform.TestCalendar
 import kotlinx.coroutines.CoroutineScope
 
@@ -16,7 +17,19 @@ fun buildKottage(
     builder: (KottageOptions.Builder.() -> Unit)? = null
 ): Pair<Kottage, TestCalendar> {
     val calendar = TestCalendar(DateTime(2022, 1, 1).utc - 1.milliseconds)
-    val environment = KottageEnvironment(KottageContext(), calendar)
+    val environment = KottageEnvironment(
+        KottageContext(),
+        calendar,
+        object : KottageLogger {
+            override suspend fun debug(message: String) {
+                println("KottageLogger DEBUG: $message")
+            }
+
+            override suspend fun error(message: String) {
+                println("KottageLogger ERROR: $message")
+            }
+        }
+    )
     val kottage = Kottage(name, directory, environment, scope, optionsBuilder = builder)
     // 初回の Event Flow が流れるようにするために、インスタンス生成後に時間を進める
     calendar.now += 1.milliseconds
