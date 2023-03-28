bfdc8d4c736c1d5923e196614a65c6c17504435b
irgaly <irgaly@gmail.com>
2023-03-28 21:42:44 +0900

    add AndroidTest

---
+++ kottage/src/androidAndroidTest/kotlin/io/github/irgaly/kottage/AndroidTest.kt
@@ -0,0 +1,62 @@
+package io.github.irgaly.kottage
+
+import io.kotest.core.spec.Spec
+import io.kotest.core.test.TestResult
+import io.kotest.engine.TestEngineLauncher
+import io.kotest.engine.listener.CollectingTestEngineListener
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import kotlin.reflect.KClass
+
+class AndroidTest {
+    @Test
+    fun kottageCacheTest() {
+        executeTest(KottageCacheTest::class)
+    }
+
+    @Test
+    fun kottageEventTest() {
+        executeTest(KottageEventTest::class)
+    }
+
+    @Test
+    fun kottageListTest() {
+        executeTest(KottageListTest::class)
+    }
+
+    @Test
+    fun kottageMigrationTest() {
+        executeTest(KottageMigrationTest::class)
+    }
+
+    @Test
+    fun kottageTest() {
+        executeTest(KottageTest::class)
+    }
+
+    private fun<T: Spec> executeTest(targetClass: KClass<T>) {
+        val listener = CollectingTestEngineListener()
+        TestEngineLauncher(listener).withClasses(targetClass).launch()
+        listener.tests.map { entry ->
+            {
+                val testCase = entry.key
+                val descriptor = testCase.descriptor.chain().joinToString(" > ") {
+                    it.id.value
+                }
+                val cause = when (val value = entry.value) {
+                    is TestResult.Error -> value.cause
+                    is TestResult.Failure -> value.cause
+                    else -> null
+                }
+                assertFalse(entry.value.isErrorOrFailure) {
+                    """$descriptor
+                            |${cause?.stackTraceToString()}""".trimMargin()
+                }
+            }
+        }.let {
+            assertAll(it)
+        }
+        println("${targetClass.simpleName} Total ${listener.tests.size}, Failure ${listener.tests.count { it.value.isErrorOrFailure }}")
+    }
+}
