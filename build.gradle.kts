f0799a11d3b2cf0b8091a62a87297a8b390a4953
irgaly <irgaly@gmail.com>
2023-03-29 11:34:55 +0900

    fix MavenPublish plugin sign

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,6 +103,9 @@ subprojects {
             )
             sign(extensions.getByType<PublishingExtension>().publications)
         }
+        tasks.withType<PublishToMavenRepository>().configureEach {
+            mustRunAfter(tasks.withType<Sign>())
+        }
     }
 }
 
